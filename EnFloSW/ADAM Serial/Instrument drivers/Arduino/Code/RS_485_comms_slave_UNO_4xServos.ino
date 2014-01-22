8e987bad07426bd0129916355f063f130a65aa5f
p.nathan82 <p.nathan82@5fe645c2-b4dc-44a5-aa5e-95b2f0996ca2>
2014-01-22 05:17:16 +0000

    Latest code for all the devices. Now they can all have their addresses reconfigured online :-)

---
+++ EnFloSW/ADAM Serial/Instrument drivers/Arduino/Code/RS_485_comms_slave_UNO_4xServos.ino
@@ -0,0 +1,220 @@
+#include <Servo.h>
+
+// Servo variables
+const int nServos = 4;
+
+Servo SERVO[nServos]; //hard-coded for Nservos
+float PWM_val;
+char PWM_bytes[4];
+
+const int PinServo[nServos] = {
+  8, 9, 10, 11};
+const int PWM_min[nServos] = {
+  900, 900, 900, 900};
+const int PWM_max[nServos] = {
+  2000, 2000, 2000, 2000};
+int PWM_range[nServos];
+int ServoNum;
+
+
+// RS-485 variables
+int PinTx_EN, Pin_frm, Pin_chk, Pin_buf;
+int L, Lm1, Lm2;
+unsigned int txDelay;
+char Buffer[12], tmp;
+
+const char thisAddress = 9;
+const long baud = 476000;
+
+
+void setup()
+{
+  ServosSetup();
+
+  SerialSetup();
+}
+
+
+void loop()
+{
+  SerialLoop();
+}
+
+
+// SLAVE CODE (ARDUINO UNO ONLY!!)
+
+// rx/tx msg syntax is 12 bytes: '@', src., dest., A/D, R/W, pin, val, val ,val, val, rxCHK, txCHK
+
+void SerialSetup()
+{
+  // Configure pins
+  PinTx_EN = 4;
+  Pin_frm = A3;
+  Pin_chk = A4;
+  Pin_buf = A5;
+
+  L = 12; // msg length
+
+  pinMode(PinTx_EN, OUTPUT);
+  pinMode(Pin_frm, OUTPUT);
+  pinMode(Pin_chk, OUTPUT);
+  pinMode(Pin_buf, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  digitalWrite(13, LOW);
+  digitalWrite(Pin_frm, LOW);
+  digitalWrite(Pin_chk, LOW);
+  digitalWrite(Pin_buf, LOW);
+  digitalWrite(PinTx_EN, LOW);  // Disable transmit mode
+
+  // Set Pin numbers to port bytes for speed
+  PinTx_EN = (1 << PIND4);
+  Pin_frm = (1 << PINC3);
+  Pin_chk = (1 << PINC4);
+  Pin_buf = (1 << PINC5);
+
+  Lm1 = L - 1;
+  Lm2 = L - 2;
+
+  txDelay = (8 * 1000000) / baud; //us
+
+  Serial.begin(baud); // send/receive messages to/from RS-485 network
+}
+
+
+void ServosSetup()
+{
+  for (int i = 0; i < nServos; i++)
+  {
+    PWM_range[i] = PWM_max[i] - PWM_min[i];
+    SERVO[i].attach(PinServo[i], PWM_min[i], PWM_max[i]);
+  }
+}
+
+
+void SerialLoop()
+{
+  // Check if message waiting in RS-485 transducer buffer
+  // if so, construct variable buffer, implement request, and send output back over RS-485 to master
+  // first check for buffer overrun, if so, turn on warning LED (pin13)
+  if (Serial.available() > 63) PORTC |= Pin_buf; 
+  while (Serial.available() >= L) // only do something if data is waiting from RS-485 network
+  {
+    if (Serial.peek() == '@') // only consider valid message start, otherwise discard
+    {
+      //memset(Buffer, 0x00, L); // initialise buffer  
+      Serial.readBytes(Buffer, L); // read msg into buffer
+
+      // only interested in messages destined for this address
+      if (Buffer[2] == thisAddress)
+      {
+        // only worth proceeding if checksum test passes, otherwise ignore instruction and return bad txChk byte
+        if (Buffer[Lm2] == static_cast<char>(CalcChecksum()))
+        {
+          // For Servos only care if Analogue Write is requested
+          switch (Buffer[3])
+          {
+          case 'A':
+            // Analogue
+            // check whether read or write
+            {
+              switch (Buffer[4])
+              {
+              case 'W':
+                // Write
+                // ########################################## Start of Servo-specific code ###################################################################
+                PWM_bytes[0] = Buffer[9];
+                PWM_bytes[1] = Buffer[8];
+                PWM_bytes[2] = Buffer[7];
+                PWM_bytes[3] = Buffer[6];
+                memcpy(&PWM_val, &PWM_bytes, sizeof(PWM_val));
+                ServoNum = static_cast<int>(Buffer[5]);
+                SERVO[ServoNum].writeMicroseconds(int((constrain(PWM_val, 0.0, 1.0) * PWM_range[ServoNum]) + PWM_min[ServoNum])); //Buffer[5] contains 'channel' (i.e. servo) no.
+                // ########################################## End of Servo-specific code #####################################################################
+                break;
+              }
+            }
+            break;
+          }
+          // now switch source and destination
+          tmp = Buffer[1];
+          Buffer[1] = Buffer[2];
+          Buffer[2] = tmp;
+
+          // to get here, txChk was okay, send back good txChk byte
+          Buffer[Lm1] = 0;
+          // calculate checksum and place on end of buffer
+          Buffer[Lm2] = static_cast<char>(CalcChecksum());
+
+          // Finally write updated buffer (now with requested value) to RS-485 network to be read by master
+          PORTD |= PinTx_EN;  // Enable transmit mode on RS-485 transceiver (Tx_EN = 1)
+
+          Serial.write(reinterpret_cast<byte*>(&Buffer), L); // write buffer to RS-485 DI
+          Serial.flush(); // wait until write complete
+          delayMicroseconds(txDelay); // don't miss out last byte!
+
+          PORTD &= ~PinTx_EN; // Disable transmit mode on RS-485 transceiver (Tx_EN = 0)
+          PORTC &= ~Pin_frm; // Successful msg cycle, reset any error lights
+          PORTC &= ~Pin_chk;
+          PORTC &= ~Pin_buf;
+        }
+        else
+        {
+          // warn of bad checksum
+          // switch source and destination
+          tmp = Buffer[1];
+          Buffer[1] = Buffer[2];
+          Buffer[2] = tmp;
+
+          // to get here, txChk was bad, send back baf txChk byte
+          Buffer[Lm1] = 1;
+          // calculate checksum and place on CHKtmp byte
+          Buffer[Lm2] = static_cast<char>(CalcChecksum());
+
+          // Finally write updated buffer (now with requested value) to RS-485 network to be read by master
+          PORTD |= PinTx_EN;  // Enable transmit mode on RS-485 transceiver (Tx_EN = 1)
+
+          Serial.write(reinterpret_cast<byte*>(&Buffer), L); // write buffer to RS-485 DI
+          Serial.flush(); // wait until write complete
+          delayMicroseconds(txDelay); // don't miss out last byte!
+
+          PORTD &= ~PinTx_EN; // Disable transmit mode on RS-485 transceiver (Tx_EN = 0)
+          PORTC |= Pin_chk; // turn on bad checksum LED
+        }
+      }
+    }
+    else
+    {
+      Serial.read(); // discard invalid byte
+      PORTC |= Pin_frm; // warn of invalid byte(s), turn on frm LED
+    }
+  } 
+}
+
+
+byte CalcChecksum()
+{
+  byte sum = 0;
+  for (int i = 0; i < Lm2; i++)
+  {
+    sum += static_cast<byte>(Buffer[i]);
+  }
+  return (sum);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- EnFloSW/ADAM Serial/Instrument drivers/Arduino/Code/RS_485_comms_slave_UNO_AD7714_10Ch_SE_therm_poll.ino
+++ EnFloSW/ADAM Serial/Instrument drivers/Arduino/Code/RS_485_comms_slave_UNO_AD7714_10Ch_SE_therm_poll.ino
@@ -1,26 +1,27 @@
 // Developed by Paul Nathan, last mod. 05/07/2013
 
 #include <SPI.h>
+#include <EEPROM.h>
 
 // Thermistor variables
 const float invB = 1.0 / 4334.0;
 const float R2 = 100000.0;
 const float R10 = 100000.0;
 const float invT0 = 1.0 / 298.15;
-const float Vs = 2.509; // hard-wired to Vref
+const float Vs = 2.531; // hard-wired to Vref
 float R1;
 
 
 // AD7714 variables
-const int pinCS[2] = {
+int Pin_CS[2] = {
   10, 9};
 
-byte chan[2];
 unsigned long DataReg, lastAutoCalib_ms, now_ms;//, GainReg, OffsetReg;
+float ADCvolts[2][5], Volts, Temperature;
+char *TemperatureBytes;
+int chan[2];
 byte ModeReg, GainBits, FilterHighBits, FilterHighReg, FilterLowReg;
 byte DRDYbit;
-float Value[2][5], Val;
-char *ValBytes;
 
 const int nChans = 5; // Hard-coded (SE = 5, DIFF = 3)
 const byte ChannelBitsLUT_DIFF[3] = {
@@ -31,47 +32,47 @@ const byte ChannelBitsLUT_SE[5] = {
 byte ChannelBits[5]; // for easy, globally selected SE or DIFF bits. Copy either of the above into this array
 
 const unsigned long AutoCalib_ms = 3600000; // reset, config, auto-calib every hour
-const float Vref = 2.499 - 0.0; // Manually input Vref (specific to each board!)
+const float Vref = 2.501 - 0.0; // Manually input Vref (specific to each board!)
 
 
 // RS-485 variables
-int pinTX_ENABLE, L, Lm1, Lm2;
+int PinTx_EN, Pin_frm, Pin_chk, Pin_buf;
+const int L = 12; // msg length
+const int Lm1 = L - 1;
+const int Lm2 = L - 2;
 unsigned int txDelay;
-char buffer[12], tmp;
+char Buffer[L], tmp;
 
-const char thisAddress = 1;
+char thisAddress = static_cast<char>(EEPROM.read(0));
 const long baud = 476000;
 
 
 void setup()
 {
-
   AD7714Setup();
 
   SerialSetup();
-
 }
 
 
 void loop()
 {
-
   // Check DRDY bit in comms register. If fallen then grab sample
   // Do not poll here as this would hold up serial comms!
 
   // CHIP 0
-  digitalWrite(pinCS[0], LOW);
+  PORTB &= ~Pin_CS[0]; // LOW
   SPI.transfer(0b00001000 | ChannelBits[chan[0]]);
   DRDYbit = SPI.transfer(0xFF) & 0b10000000;
   if (DRDYbit == 0) DRDY_fall(0);
-  digitalWrite(pinCS[0], HIGH);
+  PORTB |= Pin_CS[0]; // HIGH
 
   // CHIP 1
-  digitalWrite(pinCS[1], LOW);
+  PORTB &= ~Pin_CS[1]; // LOW
   SPI.transfer(0b00001000 | ChannelBits[chan[1]]);
   DRDYbit = SPI.transfer(0xFF) & 0b10000000;
   if (DRDYbit == 0) DRDY_fall(1);
-  digitalWrite(pinCS[1], HIGH);
+  PORTB |= Pin_CS[1]; // HIGH
 
   SerialLoop();
 
@@ -85,162 +86,63 @@ void loop()
     // Chip 1
     ConfigureAD7714(1);
   }
-
 }
+
 
 // SLAVE CODE (ARDUINO UNO ONLY!!)
 
-// rx/tx msg syntax is 12 bytes: '@', src., dest., A/D, R/W, pin, val, val ,val, val, rxCHK, txCHK
+// rx/tx msg syntax is 12 bytes: '@', src., dest., A/D/C, R/W, pin, val, val ,val, val, rxCHK, txCHK
+//                                0    1     2       3    4    5    6    7    8    9     10     11
+
 
 void SerialSetup()
 {
-  pinTX_ENABLE = 4;
-  L = 12; // msg length
+  // Configure pins
+  PinTx_EN = 4;
+  Pin_frm = A5;
+  Pin_chk = A4;
+  Pin_buf = A3;
 
-  pinMode(pinTX_ENABLE, OUTPUT);
+  pinMode(PinTx_EN, OUTPUT);
   pinMode(A5, OUTPUT);
   pinMode(A4, OUTPUT);
   pinMode(A3, OUTPUT);
 
-  digitalWrite(A5, LOW);
-  digitalWrite(A4, LOW);
-  digitalWrite(A3, LOW);
-  digitalWrite (pinTX_ENABLE, LOW);  // Disable transmit mode
+  digitalWrite(Pin_frm, LOW);
+  digitalWrite(Pin_chk, LOW);
+  digitalWrite(Pin_buf, LOW);
+  digitalWrite(PinTx_EN, LOW);  // Disable transmit mode
 
-  Lm1 = L - 1;
-  Lm2 = L - 2;
+  // Set Pin numbers to port bytes for speed
+  PinTx_EN = (1 << PIND4);
+  Pin_frm = (1 << PINC5);
+  Pin_chk = (1 << PINC4);
+  Pin_buf = (1 << PINC3);
 
   txDelay = (8 * 1000000) / baud; //us
 
   Serial.begin(baud); // send/receive messages to/from RS-485 network
 }
 
-void SerialLoop()
-{
-  // Check if message waiting in RS-485 transducer buffer
-  // if so, construct variable buffer, implement request, and send output back over RS-485 to master
-  // first check for buffer overrun, if so, turn on warning LED (pinA0)
-  if (Serial.available() > 63) digitalWrite(A5, HIGH); 
-  while (Serial.available() >= L) // only do something if data is waiting from RS-485 network
-  {
-    if (Serial.peek() == '@') // only consider valid message start, otherwise discard
-    {
-      //memset(buffer, 0x00, L); // initialise buffer  
-      Serial.readBytes(buffer, L); // read msg into buffer
 
-      // only interested in messages destined for this address
-      if (buffer[2] == thisAddress)
-      {
-        // only worth proceeding if checksum test passes, otherwise ignore instruction and return bad txChk byte
-        if (buffer[Lm2] == static_cast<char>(CalcChecksum()))
-        {
-          // For AD7714 only care if Analogue Read is requested
-          switch (buffer[3])
-          {
-          case 'A':
-            // Analogue
-            // check whether read or write
-            {
-              switch (buffer[4])
-              {
-              case 'R':
-                // Read
-                ReadFromAD7714();
-                VoltstoTemp();
-                ValBytes = reinterpret_cast<char*>(&Val);
-                buffer[6] = ValBytes[3];
-                buffer[7] = ValBytes[2];
-                buffer[8] = ValBytes[1];
-                buffer[9] = ValBytes[0];
-                break;
-              }
-            }
-            break;
-          }
-          // now switch source and destination
-          tmp = buffer[1];
-          buffer[1] = buffer[2];
-          buffer[2] = tmp;
-
-          // to get here, txChk was okay, send back good txChk byte
-          buffer[Lm1] = 0;
-          // calculate checksum and place on end of buffer
-          buffer[Lm2] = static_cast<char>(CalcChecksum());
-
-          // Finally write updated buffer (now with requested value) to RS-485 network to be read by master
-          digitalWrite (pinTX_ENABLE, HIGH);  // Enable transmit mode on RS-485 transceiver
-
-          Serial.write(reinterpret_cast<byte*>(&buffer), L); // write buffer to RS-485 DI
-          Serial.flush(); // wait until write complete
-          delayMicroseconds(txDelay); // don't miss out last byte!
-
-          digitalWrite (pinTX_ENABLE, LOW);  // Disable transmit mode on RS-485 transceiver
-          digitalWrite(A5, LOW); // Successful msg cycle, reset any error lights
-          digitalWrite(A4, LOW);
-          digitalWrite(A3, LOW);
-        }
-        else
-        {
-          // warn of bad checksum
-          // switch source and destination
-          tmp = buffer[1];
-          buffer[1] = buffer[2];
-          buffer[2] = tmp;
-
-          // to get here, txChk was bad, send back baf txChk byte
-          buffer[Lm1] = 1;
-          // calculate checksum and place on CHKtmp byte
-          buffer[Lm2] = static_cast<char>(CalcChecksum());
-
-          // Finally write updated buffer (now with requested value) to RS-485 network to be read by master
-          digitalWrite (pinTX_ENABLE, HIGH);  // Enable transmit mode on RS-485 transceiver
-
-          Serial.write(reinterpret_cast<byte*>(&buffer), L); // write buffer to RS-485 DI
-          Serial.flush(); // wait until write complete
-          delayMicroseconds(txDelay); // don't miss out last byte!
-
-          digitalWrite (pinTX_ENABLE, LOW);  // Disable transmit mode on RS-485 transceiver
-          digitalWrite(A4, HIGH);
-        }
-      }
-    }
-    else
-    {
-      Serial.read(); // discard invalid byte
-      digitalWrite(A3, HIGH); // warn of invalid byte(s)
-    }
-  } 
-
-}
-
-
-byte CalcChecksum()
-{
-  byte sum = 0;
-  for (int i = 0; i < Lm2; i++)
-  {
-    sum += static_cast<byte>(buffer[i]);
-  }
-  return (sum);
-}
-
-
-void  AD7714Setup()
+void AD7714Setup()
 {
   // configure pins
-  pinMode(pinCS[0], OUTPUT);
-  pinMode(pinCS[1], OUTPUT);
+  pinMode(Pin_CS[0], OUTPUT);
+  pinMode(Pin_CS[1], OUTPUT);
 
-  digitalWrite(pinCS[0], HIGH);
-  digitalWrite(pinCS[1], HIGH);
+  digitalWrite(Pin_CS[0], HIGH);
+  digitalWrite(Pin_CS[1], HIGH);
 
-  // configure SPI mode
+  // SPI comms
+  SPI.begin();
   SPI.setDataMode(SPI_MODE1);
   SPI.setBitOrder(MSBFIRST);
   SPI.setClockDivider(SPI_CLOCK_DIV4) ;
 
-  // begin SPI comms.
-  SPI.begin();
+  // Set Pin numbers to port bytes for speed
+  Pin_CS[0] = (1 << PINB2);
+  Pin_CS[1] = (1 << PINB1);
 
   // configure AD7714 HARDCODED HERE, check bi/uni-polar, gain, boost, sampling rate etc ##################
   FilterHighBits = 0b00000001; // 50Hz
@@ -263,62 +165,6 @@ void  AD7714Setup()
 }
 
 
-void VoltstoTemp()
-{
-  // At this point, Val is the thermistor voltage. Use it then overwrite with temp.  
-  R1 = ((Vs / Val) - 1.0) * R2;
-  Val = (1.0 / ((invB * log(R1 / R10)) + invT0)) - 273.15; // degC
-}
-
-
-void ReadFromAD7714()
-{
-  switch (buffer[5])
-  {
-  case 0:
-    // Reset ADC. Stop sampling, drive to default with >=32 DIN highs, configure and start sampling again
-    // Chip 0
-    ConfigureAD7714(0);
-    // Chip 1
-    ConfigureAD7714(1);
-    break;
-  case 1:
-    Val = Value[0][0];
-    break;
-  case 2:
-    Val = Value[0][1];
-    break;
-  case 3:
-    Val = Value[0][2];
-    break;
-  case 4:
-    Val = Value[0][3];
-    break;
-  case 5:
-    Val = Value[0][4];
-    break;
-  case 6:
-    Val = Value[1][0];
-    break;
-  case 7:
-    Val = Value[1][1];
-    break;
-  case 8:
-    Val = Value[1][2];
-    break;
-  case 9:
-    Val = Value[1][3];
-    break;
-  case 10:
-    Val = Value[1][4];
-    break;
-  default: 
-    Val = 0.0;
-  }
-
-}
-
-
 void DRDY_fall(int ChipNum)
 {     
   SPI.transfer(0b01011000 | ChannelBits[chan[ChipNum]]); // set to read data register next
@@ -326,7 +172,7 @@ void DRDY_fall(int ChipNum)
   DataReg |= static_cast<unsigned long>(SPI.transfer(0xFF)) << 8; // read mid byte and shift +8 and biwise OR
   DataReg |= static_cast<unsigned long>(SPI.transfer(0xFF)); // read low byte and bitwise OR
 
-  Value[ChipNum][chan[ChipNum]] = Vref * (float(DataReg) / 16777215.0); // raw voltage // Unipolar 24-bit mode only
+  ADCvolts[ChipNum][chan[ChipNum]] = Vref * (float(DataReg) / 16777215.0); // raw voltage // Unipolar 24-bit mode only
 
   chan[ChipNum] += 1;
   chan[ChipNum] %= nChans;  // HARD CODED FOR SINGLE-ENDED ######
@@ -340,7 +186,7 @@ void DRDY_fall(int ChipNum)
 void ConfigureAD7714(int ChipNum)
 {
   // Reset ADC
-  digitalWrite(pinCS[ChipNum], LOW);
+  PORTB &= ~Pin_CS[ChipNum]; // LOW
 
   for (int i = 0; i < 32; i++)
   {
@@ -354,25 +200,25 @@ void ConfigureAD7714(int ChipNum)
     SPI.transfer(0b00001000 | ChannelBits[i]);
     SPI.transfer(0xFF);
     ConfigureAD7714channel(ChannelBits[i]);
-    
-//    if (ChipNum == 0 && i == 0)
-//    {
-//    // Read Gain Register
-//    SPI.transfer(0b01111000 | ChannelBits[i]);
-//    GainReg = static_cast<unsigned long>(SPI.transfer(0xFF)) << 16; // read high byte and shift +16
-//    GainReg |= static_cast<unsigned long>(SPI.transfer(0xFF)) << 8; // read mid byte and shift +8 and biwise OR
-//    GainReg |= static_cast<unsigned long>(SPI.transfer(0xFF)); // read low byte and bitwise OR
-//    
-//    // Read Offset Register
-//    SPI.transfer(0b01101000 | ChannelBits[i]);
-//    OffsetReg = static_cast<unsigned long>(SPI.transfer(0xFF)) << 16; // read high byte and shift +16
-//    OffsetReg |= static_cast<unsigned long>(SPI.transfer(0xFF)) << 8; // read mid byte and shift +8 and biwise OR
-//    OffsetReg |= static_cast<unsigned long>(SPI.transfer(0xFF)); // read low byte and bitwise OR
-//    }
-    
+
+    //    if (ChipNum == 0 && i == 0)
+    //    {
+    //    // Read Gain Register
+    //    SPI.transfer(0b01111000 | ChannelBits[i]);
+    //    GainReg = static_cast<unsigned long>(SPI.transfer(0xFF)) << 16; // read high byte and shift +16
+    //    GainReg |= static_cast<unsigned long>(SPI.transfer(0xFF)) << 8; // read mid byte and shift +8 and biwise OR
+    //    GainReg |= static_cast<unsigned long>(SPI.transfer(0xFF)); // read low byte and bitwise OR
+    //    
+    //    // Read Offset Register
+    //    SPI.transfer(0b01101000 | ChannelBits[i]);
+    //    OffsetReg = static_cast<unsigned long>(SPI.transfer(0xFF)) << 16; // read high byte and shift +16
+    //    OffsetReg |= static_cast<unsigned long>(SPI.transfer(0xFF)) << 8; // read mid byte and shift +8 and biwise OR
+    //    OffsetReg |= static_cast<unsigned long>(SPI.transfer(0xFF)); // read low byte and bitwise OR
+    //    }
+
   }
 
-  digitalWrite(pinCS[ChipNum], HIGH);
+  PORTB |= Pin_CS[ChipNum]; // LOW
   //######################################
 }
 
@@ -393,19 +239,225 @@ void ConfigureAD7714channel(byte ChanBits)
   SPI.transfer(0b00010000 | ChanBits);
   SPI.transfer(ModeReg);
 
-  // Poll DRDY bit in comms register
-  do
-  {
-    SPI.transfer(0b00001000 | ChanBits);
-    DRDYbit = (SPI.transfer(0xFF) & 0b10000000); 
-  } 
-  while (DRDYbit == 0b10000000);
-  // to get here DRDY must have fallen, discard value and move on to next calib channel 
-  SPI.transfer(0b01011000 | ChanBits); // set to read data register next
-  SPI.transfer(0xFF); // read high byte and shift +16
-  SPI.transfer(0xFF); // read mid byte and shift +8 and biwise OR
-  SPI.transfer(0xFF); // read low byte and bitwise OR
+  //  Don't do the following because it prevents serial loop from running which causes timeout. Instead just return last good value until ready.
+
+  //  // Poll DRDY bit in comms register
+  //  do
+  //  {
+  //    SPI.transfer(0b00001000 | ChanBits);
+  //    DRDYbit = (SPI.transfer(0xFF) & 0b10000000); 
+  //  } 
+  //  while (DRDYbit == 0b10000000);
+  //  // to get here DRDY must have fallen, discard value and move on to next calib channel 
+  //  SPI.transfer(0b01011000 | ChanBits); // set to read data register next
+  //  SPI.transfer(0xFF); // read high byte and shift +16
+  //  SPI.transfer(0xFF); // read mid byte and shift +8 and biwise OR
+  //  SPI.transfer(0xFF); // read low byte and bitwise OR
 }
+
+
+inline float VoltstoTemp(float& V)
+{
+  R1 = ((Vs / V) - 1.0) * R2;
+  return ((1.0 / ((invB * log(R1 / R10)) + invT0)) - 273.15); // degC
+}
+
+
+inline void SerialLoop()
+{
+  // Check if message waiting in RS-485 transducer buffer
+  // if so, construct variable buffer, implement request, and send output back over RS-485 to master
+  // first check for buffer overrun, if so, turn on warning LED (pinA0)
+  if (Serial.available() > 63) PORTC |= Pin_buf;
+  while (Serial.available() >= L) // only do something if data is waiting from RS-485 network
+  {
+    if (Serial.peek() == '@') // only consider valid message start, otherwise discard
+    {
+      //memset(buffer, 0x00, L); // initialise buffer  
+      Serial.readBytes(Buffer, L); // read msg into buffer
+
+      // only interested in messages destined for this address
+      if (Buffer[2] == thisAddress)
+      {
+        // only worth proceeding if checksum test passes, otherwise ignore instruction and return bad txChk byte
+        if (Buffer[Lm2] == static_cast<char>(CalcChecksum()))
+        {
+          // For AD7714 only care if Analogue Read is requested
+          switch (Buffer[3])
+          {
+          case 'A':
+            // Analogue
+            // check whether read or write
+              switch (Buffer[4])
+              {
+              case 'R':
+                // Read ###############
+                ReadFromAD7714();
+                Temperature = VoltstoTemp(Volts);
+                TemperatureBytes = reinterpret_cast<char*>(&Temperature);
+                Buffer[6] = TemperatureBytes[3];
+                Buffer[7] = TemperatureBytes[2];
+                Buffer[8] = TemperatureBytes[1];
+                Buffer[9] = TemperatureBytes[0];
+                // #####################
+                break;
+              }
+            break;
+          case 'C':
+            // Config. mode
+            // check whether read or write
+            switch (Buffer[4])
+            {
+            case 'W':
+              // Write
+              // ########################
+              SetConfig();
+              // ########################
+            }
+            break;
+          }
+          // now switch source and destination
+          tmp = Buffer[1];
+          Buffer[1] = Buffer[2];
+          Buffer[2] = tmp;
+
+          // to get here, txChk was okay, send back good txChk byte
+          Buffer[Lm1] = 0;
+          // calculate checksum and place on end of buffer
+          Buffer[Lm2] = static_cast<char>(CalcChecksum());
+
+          // Finally write updated buffer (now with requested value) to RS-485 network to be read by master
+          PORTD |= PinTx_EN;  // Enable transmit mode on RS-485 transceiver (Tx_EN = 1)
+
+          Serial.write(reinterpret_cast<byte*>(&Buffer), L); // write buffer to RS-485 DI
+          Serial.flush(); // wait until write complete
+          delayMicroseconds(txDelay); // don't miss out last byte!
+
+          PORTD &= ~PinTx_EN; // Disable transmit mode on RS-485 transceiver (Tx_EN = 0)
+          PORTC &= ~Pin_frm; // Successful msg cycle, reset any error lights
+          PORTC &= ~Pin_chk;
+          PORTC &= ~Pin_buf;
+        }
+        else
+        {
+          // warn of bad checksum
+          // switch source and destination
+          tmp = Buffer[1];
+          Buffer[1] = Buffer[2];
+          Buffer[2] = tmp;
+
+          // to get here, txChk was bad, send back baf txChk byte
+          Buffer[Lm1] = 1;
+          // calculate checksum and place on CHKtmp byte
+          Buffer[Lm2] = static_cast<char>(CalcChecksum());
+
+          // Finally write updated buffer (now with requested value) to RS-485 network to be read by master
+          PORTD |= PinTx_EN;  // Enable transmit mode on RS-485 transceiver (Tx_EN = 1)
+
+          Serial.write(reinterpret_cast<byte*>(&Buffer), L); // write buffer to RS-485 DI
+          Serial.flush(); // wait until write complete
+          delayMicroseconds(txDelay); // don't miss out last byte!
+
+          PORTD &= ~PinTx_EN;  // Disable transmit mode on RS-485 transceiver (Tx_EN = 0)
+          PORTC |= Pin_chk; // turn on bad checksum LED
+        }
+      }
+    }
+    else
+    {
+      Serial.read(); // discard invalid byte
+      PORTC |= Pin_frm; // warn of invalid byte(s), turn on frm LED
+    }
+  } 
+
+}
+
+
+void SetConfig()
+{
+  const int Chan = static_cast<int>(static_cast<byte>(Buffer[5]));
+  const byte val = static_cast<byte>(ValBytesToFloat());
+
+  if (Chan == 255) // set address
+  {
+    thisAddress = static_cast<char>(val);
+    EEPROM.write(0, val);
+  }
+}
+
+
+inline byte CalcChecksum()
+{
+  byte sum = 0;
+  for (int i = 0; i < Lm2; i++)
+  {
+    sum += static_cast<byte>(Buffer[i]);
+  }
+  return sum;
+}
+
+
+inline float ValBytesToFloat()
+{
+  char val_bytes[4];
+  float val;
+
+  val_bytes[0] = Buffer[9];
+  val_bytes[1] = Buffer[8];
+  val_bytes[2] = Buffer[7];
+  val_bytes[3] = Buffer[6];
+  memcpy(&val, &val_bytes, 4);
+
+  return val;
+}
+
+
+void ReadFromAD7714()
+{
+  switch (static_cast<int>(static_cast<byte>(Buffer[5])))
+  {
+  case 0:
+    // Reset ADC. Stop sampling, drive to default with >=32 DIN highs, configure and start sampling again
+    // Chip 0
+    ConfigureAD7714(0);
+    // Chip 1
+    ConfigureAD7714(1);
+    break;
+  case 1:
+    Volts = ADCvolts[0][0];
+    break;
+  case 2:
+    Volts = ADCvolts[0][1];
+    break;
+  case 3:
+    Volts = ADCvolts[0][2];
+    break;
+  case 4:
+    Volts = ADCvolts[0][3];
+    break;
+  case 5:
+    Volts = ADCvolts[0][4];
+    break;
+  case 6:
+    Volts = ADCvolts[1][0];
+    break;
+  case 7:
+    Volts = ADCvolts[1][1];
+    break;
+  case 8:
+    Volts = ADCvolts[1][2];
+    break;
+  case 9:
+    Volts = ADCvolts[1][3];
+    break;
+  case 10:
+    Volts = ADCvolts[1][4];
+    break;
+  default: 
+    Volts = 0.0;
+  }
+}
+
 
 
 
