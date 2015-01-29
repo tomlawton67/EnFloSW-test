8ad74dd664dbb8b2fcf1eebe76845c0923b31dbe
tomlawton67 <18744442+tomlawton67@users.noreply.github.com>
2015-01-29 22:49:08 +0000

    Testing branch synchronisation (FROM trunk TO branch)

--- X-ClosedBranches/ExecutableLauncher/ADAM Serial/Instrument drivers/Arduino/Code/RS_485_comms_Master.ino
+++ X-ClosedBranches/ExecutableLauncher/ADAM Serial/Instrument drivers/Arduino/Code/RS_485_comms_Master.ino
@@ -1,39 +1,40 @@
-// Developed by Paul Nathan, last mod. 05/07/2013
+// Developed by Paul Nathan, last mod. 18/01/2014
 
-int pinTX_ENABLE, L, Lm1, Lm2;
+int PinTx_EN;
 unsigned int txDelay;
 
-char buffer[12];
+const int L = 12; // msg length
+const int Lm1 = L - 1;
+const int Lm2 = L - 2;
+char Buffer[L];
 
 const char thisAddress = 0;
 const long baud = 476000;
 
+
 // MASTER DUE CODE
 
-// rx/tx msg syntax is 12 bytes: '@', src., dest., A/D, R/W, pin, val, val ,val, val, rxCHK, txCHK
+// rx/tx msg syntax is 12 bytes: '@', src., dest., A/D/C, R/W, pin, val, val ,val, val, rxCHK, txCHK
+//                                0    1     2       3    4    5    6    7    8    9     10     11
 
 
 void setup()
 {
-  pinTX_ENABLE = 2;
-  L = 12; // msg length
+  PinTx_EN = 2;
 
-  pinMode(pinTX_ENABLE, OUTPUT);
+  pinMode(PinTx_EN, OUTPUT);
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
 
-  pinMode(A11, OUTPUT);
-  pinMode(A10, OUTPUT);
-  pinMode(A9, OUTPUT);
+  pinMode(A11, OUTPUT); // buf
+  pinMode(A10, OUTPUT); // chk
+  pinMode(A9, OUTPUT); // frm
 
   digitalWrite(A11, LOW);
   digitalWrite(A10, LOW);
   digitalWrite(A9, LOW);
 
-  digitalWrite(pinTX_ENABLE, LOW);  // Disable transmit mode
-
-  Lm1 = L - 1;
-  Lm2 = L - 2;
+  digitalWrite(PinTx_EN, LOW);  // Disable transmit mode
 
   txDelay = (8 * 1000000) / baud; //us
 
@@ -54,18 +55,18 @@ void loop()
     if (Serial.peek() == '@') // only consider valid message start, otherwise discard
     {
       //memset(buffer, 0x00, L); // initialise buffer  
-      Serial.readBytes(buffer, L); // read msg into buffer
+      Serial.readBytes(Buffer, L); // read msg into buffer
 
       // calculate checksum and place on end of buffer
-      buffer[Lm2] = static_cast<char>(CalcChecksum());
+      Buffer[Lm2] = static_cast<char>(CalcChecksum());
 
-      digitalWrite (pinTX_ENABLE, HIGH);  // Enable transmit mode on RS-485 transceiver
+      digitalWrite(PinTx_EN, HIGH);  // Enable transmit mode on RS-485 transceiver
 
-      Serial1.write(reinterpret_cast<byte*>(&buffer), L); // write buffer to RS-485
+      Serial1.write(reinterpret_cast<byte*>(&Buffer), L); // write buffer to RS-485
       Serial1.flush(); // wait until write complete
       delayMicroseconds(txDelay); // don't miss out last byte!
 
-      digitalWrite (pinTX_ENABLE, LOW);  // Disable transmit mode on RS-485 transceiver
+      digitalWrite(PinTx_EN, LOW);  // Disable transmit mode on RS-485 transceiver
       digitalWrite(A11, LOW); // Successful msg cycle, reset any error lights
       digitalWrite(A10, LOW);
       digitalWrite(A9, LOW);
@@ -86,24 +87,24 @@ void loop()
     if (Serial1.peek() == '@') // only consider valid message start, otherwise discard
     {
       //memset(buffer, 0x00, L); // initialise buffer  
-      Serial1.readBytes(buffer, L); // read msg into buffer (excludes char argument!) 
+      Serial1.readBytes(Buffer, L); // read msg into buffer (excludes char argument!) 
 
       // only interested in messages destined for this address
-      if (buffer[2] == thisAddress)
+      if (Buffer[2] == thisAddress)
       {
         // Check if rx checksum equals calculated checksum, 0 good, 1 bad
-        if (buffer[Lm2] == static_cast<char>(CalcChecksum()))
+        if (Buffer[Lm2] == static_cast<char>(CalcChecksum()))
         {
-          buffer[Lm2] = 0;
+          Buffer[Lm2] = 0;
           digitalWrite(A10, LOW);
         }
         else
         {
-          buffer[Lm2] = 1;
+          Buffer[Lm2] = 1;
           digitalWrite(A10, HIGH);
         }
 
-        Serial.write(reinterpret_cast<byte*>(&buffer), L); // write buffer to PC
+        Serial.write(reinterpret_cast<byte*>(&Buffer), L); // write buffer to PC
         Serial.flush(); // wait until write complete (no need for txDelay on this serial)
 
         digitalWrite(A11, LOW); // Successful msg cycle, reset any error lights
@@ -120,29 +121,15 @@ void loop()
 }
 
 
-byte CalcChecksum()
+inline byte CalcChecksum()
 {
   byte sum = 0;
   for (int i = 0; i < Lm2; i++)
   {
-    sum += static_cast<byte>(buffer[i]);
+    sum += static_cast<byte>(Buffer[i]);
   }
-  return (sum);
+  return sum;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
